aa5827467c8fe906d666308e40e4f8888042308e
Huy <phamgiahuytkd@gmail.com>
2025-08-15 13:33:32 +0000

    v5.6

--- src/service/api.jsx
+++ src/service/api.jsx
@@ -4,7 +4,7 @@ import {jwtDecode} from "jwt-decode";
 
 // Cấu hình cơ bản cho axios
 const api = axios.create({
-  baseURL: "https://icommerce-production.up.railway.app", // Địa chỉ của backend
+  baseURL: "https://icommerce-production.up.railway.app/iCommerce", // Địa chỉ của backend
   headers: {
     "Content-Type": "application/json",
   },
